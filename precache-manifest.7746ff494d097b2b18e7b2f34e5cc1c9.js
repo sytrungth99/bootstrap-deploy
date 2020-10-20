974ecf39e56b8f50ff0240ec8926f9049648f643
unknown <sytrungth99a@gmail.com>
2020-10-20 17:35:01 +0700

    Updates

---
+++ precache-manifest.7746ff494d097b2b18e7b2f34e5cc1c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e269f576abe10ad49b2aefebc4cc75e8",
+    "url": "/bootstrap-deploy/index.html"
+  },
+  {
+    "revision": "cfb89859de3b1799af9e",
+    "url": "/bootstrap-deploy/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "e8aa1e17118715f56c35",
+    "url": "/bootstrap-deploy/static/css/main.679c08fa.chunk.css"
+  },
+  {
+    "revision": "cfb89859de3b1799af9e",
+    "url": "/bootstrap-deploy/static/js/2.55f62499.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bootstrap-deploy/static/js/2.55f62499.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8aa1e17118715f56c35",
+    "url": "/bootstrap-deploy/static/js/main.3eda73c4.chunk.js"
+  },
+  {
+    "revision": "e5bd2fa6d9e3bf6b9376",
+    "url": "/bootstrap-deploy/static/js/runtime-main.4fc396f1.js"
+  }
+]);
